8c06dda389f073e8d6cc1923ddccbb9e3cd8f273
Oleksandr Kuzmenko <oleksandrkuzmenko@Oleksandrs-MacBook-Pro.local>
2022-06-08 10:54:30 +0300

    (FIX) debug #6

--- push.sh
+++ push.sh
@@ -148,13 +148,12 @@ case "$2" in
 
         echo "Pushing $CHART to repo $REPO_URL..."
         #HTTP_STATUS_CODE=$(curl -is -u "$AUTH" -w "%{http_code}" "$REPO_URL" --upload-file "$CHART_PACKAGE" | indent)
-        #if [ $HTTP_STATUS_CODE != "200" ]; then
-        #    echo "Cannot upload chart $CHART to helm registry. HTTP status: ${HTTP_STATUS_CODE}"
-        #    echo "Error"
-        #    exit 1
-        #else
-        #    echo "Chart $CHART was successfully uploaded to helm registry $REPO_URL."
-        curl -is -u "$AUTH" "$REPO_URL" --upload-file "$CHART_PACKAGE" | indent
+        HTTP_STATUS_CODE=$(curl -is -u "$AUTH" -w "%{http_code}" "$REPO_URL" --upload-file "$CHART_PACKAGE")
+        if [[ $HTTP_STATUS_CODE -ne 200 ]]; then
+            echo "Cannot upload chart $CHART_PACKAGE to helm registry. HTTP status: ${HTTP_STATUS_CODE}"
+            exit 1
+        else
+            echo "Chart $CHART_PACKAGE was successfully uploaded to helm registry $REPO_URL."
         echo "Done"
         ;;
 esac
